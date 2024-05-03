ffe713fc5951cb52a3e30c926e04bba4476ccf25
Lucas Silva <lucas.silva182@outlook.com>
2024-05-03 00:47:26 -0300

    feat: add server action for login and register page, and create form for both

---
+++ actions/login.ts
@@ -0,0 +1,14 @@
+'use server';
+import * as z from 'zod';
+import { LoginSchema } from '@/schemas';
+
+export const login = async (values: z.infer<typeof LoginSchema>) => {
+  const validatedFields = LoginSchema.safeParse(values);
+
+  if (!validatedFields.success) {
+    // throw new Error('Invalid fields');
+    return { error: 'Invalid fields!' };
+  }
+
+  return { success: 'Email sent!' };
+};
