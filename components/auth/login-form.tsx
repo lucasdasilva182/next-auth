5a8d178b74a9d2e3b369bd72246ae1d3b9a66340
Lucas Silva <lucas.silva182@outlook.com>
2024-05-10 09:07:23 -0300

    feat: add Google and Github provider

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -5,6 +5,8 @@ import { CardWrapper } from '@/components/auth/card-wrapper';
 import { useForm } from 'react-hook-form';
 import { LoginSchema } from '@/schemas';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { useSearchParams } from 'next/navigation';
+
 import {
   Form,
   FormControl,
@@ -21,6 +23,12 @@ import { login } from '@/actions/login';
 import { useState, useTransition } from 'react';
 
 export const LoginForm = () => {
+  const searchParams = useSearchParams();
+  const urlError =
+    searchParams.get('error') == 'OAuthAccountNotLinked'
+      ? 'Email already in use with different provider!'
+      : '';
+
   const [error, setError] = useState<string | undefined>('');
   const [success, setSuccess] = useState<string | undefined>('');
   const [isPending, startTransition] = useTransition();
@@ -39,8 +47,9 @@ export const LoginForm = () => {
 
     startTransition(() => {
       login(values).then((data) => {
-        setError(data.error);
-        setSuccess(data.success);
+        setError(data?.error);
+        //TODO: Add when we add 2FA
+        // setSuccess(data?.success);
       });
     });
   };
@@ -91,7 +100,7 @@ export const LoginForm = () => {
               )}
             />
           </div>
-          <FormError message={error} />
+          <FormError message={error || urlError} />
           <FormSuccess message={success} />
           <Button disabled={isPending} type="submit" className="w-full">
             Login
---
+++ middleware.ts
@@ -0,0 +1,41 @@
+import NextAuth from 'next-auth';
+
+import authConfig from '@/auth.config';
+import {
+  DEFAULT_LOGIN_REDIRECT,
+  authRoutes,
+  apiAuthPrefix,
+  publicRoutes,
+} from '@/routes';
+
+const { auth } = NextAuth(authConfig);
+
+export default auth((req) => {
+  const { nextUrl } = req;
+  const isLoggedIn = !!req.auth;
+
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+
+  if (isApiAuthRoute) {
+    return undefined;
+  }
+
+  if (isAuthRoute) {
+    if (isLoggedIn) {
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+    }
+    return undefined;
+  }
+
+  if (!isLoggedIn && !isPublicRoute) {
+    return Response.redirect(new URL('/auth/login', nextUrl));
+  }
+
+  return undefined;
+});
+
+export const config = {
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+};
