4dddcb48128772b8a0361c20b063d8b948b37f80
Lucas Silva <lucas.silva182@outlook.com>
2024-05-20 09:32:49 -0300

    feat: add Form to update informations in the SettingsPage

--- auth.ts
+++ auth.ts
@@ -6,6 +6,7 @@ import authConfig from '@/auth.config';
 import { getUserById } from '@/data/user';
 import { UserRole } from '@prisma/client';
 import { getTwoFactorConfirmationByUserId } from './data/two-factor-confirmation';
+import { getAccountByUserId } from './data/account';
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
   pages: {
@@ -58,6 +59,12 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
         session.user.isTwoFactorEnabled = token.isTwoFactorEnabled as boolean;
       }
 
+      if (session.user) {
+        session.user.name = token.name;
+        session.user.email = token.email as string;
+        session.user.isOAuth = token.isOAuth as boolean;
+      }
+
       return session;
     },
     async jwt({ token }) {
@@ -67,6 +74,11 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
 
       if (!existingUser) return token;
 
+      const existingAccount = await getAccountByUserId(existingUser.id);
+
+      token.isOAuth = !!existingAccount;
+      token.name = existingUser.name;
+      token.email = existingUser.email;
       token.role = existingUser.role;
       token.isTwoFactorEnabled = existingUser.isTwoFactorEnabled;
 
